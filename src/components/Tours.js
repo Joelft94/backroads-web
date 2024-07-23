d4476baebcac4b393e597f03b49ca008a888e6bc
Joel Ferreira <joel_ferrer_94@hotmail.com>
2024-07-23 01:24:42 -0400

    add Pagelink, Service, Socialink, Tour components

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,5 +1,6 @@
 import Title from "./Title"
 import {tours} from '../data'
+import Tour from "./Tour"
 
 const Tours = () => {
     return (
@@ -7,29 +8,8 @@ const Tours = () => {
             <Title titleFirstWord= 'our' titleSecondWord= 'tours' />
             <div className="section-center featured-center">
             {tours.map((tour)=>{
-            const {id, img, date, title, text, location, duration, price} = tour
             return (
-                <article className="tour-card" key= {id}>
-                    <div className="tour-img-container">
-                        <img src={img} className="tour-img" alt="" />
-                        <p className="tour-date">{date}</p>
-                    </div>
-                    <div className="tour-info">
-                        <div className="tour-title">
-                            <h4>{title}</h4>
-                        </div>
-                        <p>
-                            {text}
-                        </p>
-                        <div className="tour-footer">
-                            <p>
-                                <span><i className="fas fa-map"></i></span> {location}
-                            </p>
-                            <p>{duration}</p>
-                            <p>{price}</p>
-                        </div>
-                    </div>
-                </article>
+                <Tour key={tour.id} {...tour} />
 
             )})
             }
